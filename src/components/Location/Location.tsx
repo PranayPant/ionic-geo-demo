f2e6eb9007267bfc2643f05ec47f1a639d7793e7
PranayPant <ppantg1@gmail.com>
2023-02-25 17:44:46 -0600

    added google maps

--- src/components/Location/Location.tsx
+++ src/components/Location/Location.tsx
@@ -1,10 +1,11 @@
 import { Geolocation, Position } from "@capacitor/geolocation";
+import { GoogleMap } from "@capacitor/google-maps";
 import {
   IonPage,
-  IonItem,
-  IonList,
   useIonViewWillEnter,
   IonContent,
+  IonButton,
+  useIonViewWillLeave,
 } from "@ionic/react";
 import * as React from "react";
 
@@ -12,15 +13,45 @@ import "./Location.css";
 
 const Location: React.FC = () => {
   const [location, setLocation] = React.useState<Position | null>(null);
+  const [map, setMap] = React.useState<GoogleMap | null>(null);
+  const mapRef = React.useRef<HTMLDivElement>(null);
+
+  React.useEffect(() => {
+    if (!mapRef?.current || !location?.coords) return;
+    const { coords } = location;
+    GoogleMap.create({
+      id: "map-2",
+      element: mapRef.current,
+      apiKey: "AIzaSyBESUC0Dw1LKX7Nvw5RWVLz43bxmn8OrSk",
+      config: {
+        center: {
+          lat: coords.latitude,
+          lng: coords.longitude,
+        },
+        zoom: 12,
+      },
+    })
+      .then((map) => setMap(map))
+      .catch((err) => console.error(err));
+  }, [location]);
 
   useIonViewWillEnter(() => {
-    Geolocation.getCurrentPosition({enableHighAccuracy: false})
-      .then((coords) => setLocation(coords))
-      .catch((err) => console.error(err));
+    const initializeLocationService = async () => {
+      try {
+        const loc = await Geolocation.getCurrentPosition();
+        setLocation(loc);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    initializeLocationService();
+  });
+  useIonViewWillLeave(() => {
+    map?.destroy();
   });
   return (
     <IonPage>
-      <IonContent>
+      <IonContent >
         <div className="flex-col center location-page">
           <span>My location is currently</span>
           {location && (
@@ -29,6 +60,7 @@ const Location: React.FC = () => {
               <span>Longitude: {location?.coords.longitude}</span>
             </div>
           )}
+          <capacitor-google-map ref={mapRef} />
         </div>
       </IonContent>
     </IonPage>
