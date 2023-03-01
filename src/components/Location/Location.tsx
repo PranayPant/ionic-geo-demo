2ff4deb35c9a09af6c4795a18d2bcff9445097a4
PranayPant <ppantg1@gmail.com>
2023-02-28 19:25:20 -0600

    added markers and clustering

--- src/components/Location/Location.tsx
+++ src/components/Location/Location.tsx
@@ -1,4 +1,9 @@
-import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
+import {
+  GoogleMap,
+  useJsApiLoader,
+  Marker,
+  MarkerClusterer,
+} from "@react-google-maps/api";
 import {
   IonPage,
   useIonViewWillEnter,
@@ -23,9 +28,6 @@ const Location: React.FC = () => {
   const [nearbyUsers, setNearbyUsers] = React.useState<
     RedisGeoSearchResult[] | null
   >(null);
-  const [markers, setMarkers] = React.useState<
-    { markerId: string; userId: string }[]
-  >([]);
   const [map, setMap] = React.useState<google.maps.Map | null>(null);
   const [ws, setWs] = React.useState<WebSocket | null>(null);
   const { isLoaded } = useJsApiLoader({
@@ -34,10 +36,7 @@ const Location: React.FC = () => {
   });
 
   const onLoad = React.useCallback((map: google.maps.Map) => {
-    const bounds = new window.google.maps.LatLngBounds(coords);
-    map.fitBounds(bounds);
     setMap(map);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const onUnmount = React.useCallback(() => {
@@ -45,74 +44,35 @@ const Location: React.FC = () => {
   }, []);
 
   // Add nearby users
-  // React.useEffect(() => {
-  //   if (!nearbyUsers || !map) return;
-  //   map
-  //     .addMarkers(
-  //       nearbyUsers.map((d) => ({
-  //         title: d.member,
-  //         coordinate: {
-  //           lat: parseFloat(`${d.coordinates.latitude}`),
-  //           lng: parseFloat(`${d.coordinates.longitude}`),
-  //         },
-  //       }))
-  //     )
-  //     .then((ids) =>
-  //       setMarkers(
-  //         nearbyUsers.map((u, i) => ({ markerId: ids[i], userId: u.member }))
-  //       )
-  //     )
-  //     .catch((err) => console.error(err));
-  // }, [nearbyUsers, map]);
+  React.useEffect(() => {
+    if (!nearbyUsers || !map) return;
+    const bounds = new google.maps.LatLngBounds();
+    nearbyUsers.forEach((u) => {
+      bounds.extend({
+        lat: parseFloat(`${u.coordinates.latitude}`),
+        lng: parseFloat(`${u.coordinates.longitude}`),
+      });
+    });
+    map.fitBounds(bounds);
+  }, [nearbyUsers, map]);
 
   // Add user position and initialize search
-  // React.useEffect(() => {
-  //   if (!map || !location?.coords) return;
-  //   map.setOnMarkerClickListener((data) => console.log("Listening", data));
-  //   map
-  //     .addMarker({
-  //       title: "My position",
-  //       coordinate: {
-  //         lat: location.coords.latitude,
-  //         lng: location.coords.longitude,
-  //       },
-  //     })
-  //     .then((res) => res)
-  //     .catch((err) => console.error(err));
-  //   ws?.send(
-  //     JSON.stringify({
-  //       id: nanoid(),
-  //       type: "ready-to-search",
-  //       data: {
-  //         longitude: location?.coords.longitude,
-  //         latitude: location?.coords.latitude,
-  //         member: MEMBER_ID,
-  //         radius: RADIUS,
-  //         radiusUnit: UNITS,
-  //       },
-  //     })
-  //   );
-  // }, [ws, map, location?.coords]);
-
-  // Initialize map
-  // React.useLayoutEffect(() => {
-  //   if (!mapRef?.current || !location?.coords) return;
-  //   const { coords } = location;
-  //   GoogleMap.create({
-  //     id: "map",
-  //     element: mapRef.current,
-  //     apiKey: "AIzaSyBESUC0Dw1LKX7Nvw5RWVLz43bxmn8OrSk",
-  //     config: {
-  //       center: {
-  //         lat: coords.latitude,
-  //         lng: coords.longitude,
-  //       },
-  //       zoom: 7,
-  //     },
-  //   })
-  //     .then((map) => setMap(map))
-  //     .catch((err) => console.error(err));
-  // }, [location]);
+  React.useEffect(() => {
+    if (!map || !coords || !ws) return;
+    ws.send(
+      JSON.stringify({
+        id: nanoid(),
+        type: "ready-to-search",
+        data: {
+          longitude: coords.lng,
+          latitude: coords.lat,
+          member: MEMBER_ID,
+          radius: RADIUS,
+          radiusUnit: UNITS,
+        },
+      })
+    );
+  }, [ws, map, coords]);
 
   useIonViewWillEnter(() => {
     const initializeMessaging = async () => {
@@ -132,6 +92,7 @@ const Location: React.FC = () => {
           switch (type) {
             case "connect": {
               console.log("Server: connect -->", data);
+              setWs(ws);
               break;
             }
             case "initialized-user": {
@@ -166,7 +127,6 @@ const Location: React.FC = () => {
               console.error("Received invalid message type:", type);
           }
         });
-        setWs(ws);
       } catch (err) {
         console.error("Error Initializing Location:", JSON.stringify(err));
       }
@@ -191,36 +151,42 @@ const Location: React.FC = () => {
     );
   };
 
-  // const handleRemoveMarkers = () => {
-  //   if (!map) return;
-  //   map.removeMarkers(markers.map((m) => m.markerId));
-  // };
-
   return (
     <IonPage>
       <IonContent>
         <section className="flex-col center">
-          {isLoaded && coords && map && (
+          {isLoaded && coords && (
             <>
               <GoogleMap
-                mapContainerStyle={{ width: 400, height: 400 }}
+                mapContainerStyle={{ width: 700, height: 700 }}
                 center={coords}
-                zoom={10}
+                zoom={12}
                 onLoad={onLoad}
                 onUnmount={onUnmount}
               >
                 {/* Child components, such as markers, info windows, etc. */}
-                <Marker position={coords} />
-                {nearbyUsers?.map((u) => (
-                  <Marker
-                    position={
-                      {
-                        lat: u.coordinates.latitude,
-                        lng: u.coordinates.longitude,
-                      } as google.maps.LatLngLiteral
-                    }
-                  />
-                ))}
+                <Marker title={MEMBER_ID} position={coords} />
+                <MarkerClusterer>
+                  {(clusterer) => (
+                    <>
+                      {nearbyUsers
+                        ?.filter((u) => u.member !== MEMBER_ID)
+                        .map((u) => (
+                          <Marker
+                            key={u.member}
+                            clusterer={clusterer}
+                            title={u.member}
+                            position={
+                              {
+                                lat: parseFloat(`${u.coordinates.latitude}`),
+                                lng: parseFloat(`${u.coordinates.longitude}`),
+                              } as google.maps.LatLngLiteral
+                            }
+                          />
+                        ))}
+                    </>
+                  )}
+                </MarkerClusterer>
               </GoogleMap>
               <IonButton onClick={handleSearch}>Search</IonButton>
             </>
